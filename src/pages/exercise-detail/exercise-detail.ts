8d57b426e98d4da8aba0b97b88daea832c0b26c3
Tanya Matthews <t.matthews@kainos.com>
2019-02-04 21:07:12 +0000

    Exercise descriptions now fully pulling down and correct images now displaying.

--- src/pages/exercise-detail/exercise-detail.ts
+++ src/pages/exercise-detail/exercise-detail.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RestProvider } from '../../providers/rest/rest';
 
-
 @IonicPage()
 @Component({
   selector: 'page-exercise-detail',
@@ -13,25 +12,42 @@ export class ExerciseDetailPage {
   ex: any;
   exerciseImageData: any;
   exerciseImageString: string;
+
+  exercises: any;
+  muscles: any;
+  second_muscles: any;
+  equipment: any;
+
   id: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public restProvider: RestProvider) {
   	this.ex = navParams.get('data');
+  	this.getExerciseDetail();
+
   }
 
   ionViewDidLoad() {
-    console.log("ID: " + this.ex.category.id);
     this.getImage();
-  }
+    this.getExerciseDetail();
+  } 
 
   getImage(){
   	console.log("DA ID " + this.ex.id);
-  	this.id = this.ex.id;
-    this.restProvider.getExerciseImage(this.id).then(data =>{
-      this.exerciseImageData = data;
-      console.log(this.exerciseImageData.image);
-      this.exerciseImageString = this.exerciseImageData.image;
+    this.restProvider.getExerciseImage(this.ex.id).then(data =>{
+      this.exerciseImageData = data.results;
+      //this.exerciseImageString = this.exerciseImageData.image;
     });
+  }
+
+  getExerciseDetail(){
+  	console.log("ID = " + this.ex.id);
+  	this.restProvider.getExerciseMoreData(this.ex.id).then(data =>{
+  		console.log(data);
+  		this.exercises = data;
+  		this.muscles = data.muscles;
+  		this.second_muscles = data.secondary_muscles;
+  		this.equipment = data.equipment;
+  	});
   }
 
 }
--- src/providers/rest/rest.ts
+++ src/providers/rest/rest.ts
@@ -7,8 +7,10 @@ import { Injectable } from '@angular/core';
 export class RestProvider {
 
 	apiUrl = "https://jsonplaceholder.typicode.com";
-	baseExerciseUrl = "https://wger.de/api/v2/exercise/info?format=json&language=2";
-  baseExerciseImgUrl = "https://wger.de/api/v2/exerciseimage/";
+  format = "format=json&language=2";
+	exerciseUrl = "https://wger.de/api/v2/exercise?format=json&language=2&limit=100&status=2";
+  exerciseMoreInfoUrl = "https://wger.de/api/v2/exerciseinfo/"
+  exerciseImgUrl = "https://wger.de/api/v2/exerciseimage/?exercise=";
   url = "";
 
 	constructor(public http: HttpClient) {
@@ -22,7 +24,7 @@ ionViewDidLoad(){
 
 getExercises(){
   return new Promise(resolve => {
-    this.http.get(this.baseExerciseUrl).subscribe(data => {
+    this.http.get(this.exerciseUrl).subscribe(data => {
       resolve(data);
     }, err => {
       console.log(err);
@@ -32,17 +34,14 @@ getExercises(){
 
 getExercisesWithFilter(type: String, equipment: String){
   //build url based on parameter values
-  console.log(equipment);
-  console.log(type);
-
   if(equipment != undefined && type != undefined){
-    this.url = this.baseExerciseUrl + "&equipment=" + equipment + "&muscles=" + type;
+    this.url = this.exerciseUrl + "&equipment=" + equipment + "&muscles=" + type;
   }else if(equipment == undefined && type != ""){
-    this.url = this.baseExerciseUrl + "&muscles=" + type;
+    this.url = this.exerciseUrl + "&muscles=" + type;
   }else if(equipment != "" && type == undefined){
-    this.url = this.baseExerciseUrl + "&equipment=" + equipment;
+    this.url = this.exerciseUrl + "&equipment=" + equipment;
   }else if(equipment == undefined && type == undefined){
-    this.url = this.baseExerciseUrl;
+    this.url = this.exerciseUrl;
   }
 
   console.log(this.url);
@@ -59,7 +58,7 @@ getExercisesWithFilter(type: String, equipment: String){
   getExerciseImage(id: any){
     //get image for exercise id
     console.log(id);
-    this.url = this.baseExerciseImgUrl + id + "/";
+    this.url = this.exerciseImgUrl + id + "&" + this.format;
     console.log(this.url);
     return new Promise(resolve => {
     this.http.get(this.url).subscribe(data => {
@@ -68,4 +67,16 @@ getExercisesWithFilter(type: String, equipment: String){
       console.log(err);
     });
   });}
+
+  getExerciseMoreData(id:any){
+    this.url = this.exerciseMoreInfoUrl + id + "/?" + this.format;
+    console.log(this.url);
+        return new Promise(resolve => {
+    this.http.get(this.url).subscribe(data => {
+      resolve(data);
+    }, err => {
+      console.log(err);
+    });
+  });}
+
 }
